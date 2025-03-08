972b01f827a79c5f2e7f1f0f6b8f4f9429cbf7dd
LIK WAI NG <nglikwai@ymail.com>
2025-03-08 21:31:05 +0000

    fix: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export const items = [
 
 export default () => {
   const commonStyle =
-    'text-4xl flex font-bold items-center flex-col justify-center gap-10 cursor-pointer transition hover:bg-border';
+    'sm:text-4xl flex text-2xl font-bold items-center flex-col justify-center gap-10 cursor-pointer transition hover:bg-border';
   return (
     <div className='container mx-auto border-x border-dashed grid sm:grid-cols-2 max-ava-h'>
       <Link href='pp' className={cn(commonStyle, 'border-b sm:border-none')}>
